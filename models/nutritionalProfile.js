faba0b502e3f10e74e2f9e88eff25fc8cfbdfdf1
GreatDeveloper66 <ashaffe@gmail.com>
2026-02-17 17:13:44 -0600

    nutritional profile schema

---
+++ models/nutritionalProfile.js
@@ -0,0 +1,13 @@
+import { mongoose } from 'mongoose';
+
+const nutritionalProfileSchema = new mongoose.Schema({
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    age: { type: Number, required: true },
+    sex: { type: String, enum: ['male', 'female'], required: true },
+    weight: { type: Number, required: true }, // in kg
+    height: { type: Number, required: true }, // in cm
+    activityLevel: { type: String, enum: ['sedentary', 'light', 'moderate', 'active', 'very active'], required: true },
+    dietaryPreferences: { type: [String], enum: ['Normal', 'Keto', 'High Protein', 'Vegetarian', 'Vegan'], default: ['none'] },
+    budgetLevel: { type: String, enum: ['Minimal', 'Normal', 'Premium'], required: true },
+    createdAt: { type: Date, default: Date.now },
+});
