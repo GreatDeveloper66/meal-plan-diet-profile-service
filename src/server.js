663c4203bf35b1e51e7eeb5efd45e9dabb8413d1
GreatDeveloper66 <ashaffe@gmail.com>
2026-02-17 20:43:12 -0600

    Add nutritonal profile routes

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import { createNutritionalProfile, getNutritionalProfile, updateNutritionalProfile } from "./controllers/nutritionalProfileController.js";
 
 dotenv.config();
 connectDB();
@@ -16,6 +17,15 @@ app.use(express.json());
 app.get("/api/health", (req, res) => {
   res.json({ status: "ok" });
 });
+
+app.get("/api/hello", (req, res) => {
+  res.json({ message: "Hello, World!" });
+});
+
+app.post("/api/create-nutritional-profile", createNutritionalProfile);
+app.get("/api/get-nutritional-profile", getNutritionalProfile);
+app.patch("/api/update-nutritional-profile", updateNutritionalProfile);
+
 
 
 export default app;
