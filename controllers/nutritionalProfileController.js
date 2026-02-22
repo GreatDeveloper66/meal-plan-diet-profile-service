019b2892ad9db4c2cb7091c8588b7f0efea8427f
GreatDeveloper66 <ashaffe@gmail.com>
2026-02-22 15:08:14 -0600

    update array index to zero in authorization bearer in create nutritional profile method

--- controllers/nutritionalProfileController.js
+++ controllers/nutritionalProfileController.js
@@ -16,7 +16,7 @@ const verifyJwtToken = (token) => {
 //Create a nutritional profile by jwt token
 export const createNutritionalProfile = async (req, res) => {
     try {
-        const token = req.headers.authorization.split(" ")[1];
+        const token = req.headers.authorization.split(" ")[0];
         if (!token) {
             return res.status(401).json({ message: 'No token provided' });
         }
